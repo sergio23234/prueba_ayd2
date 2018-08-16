a8e432f273eec284d9a30e829322b8ac6aca8009
wilsonellolo <wilsonellolo@gmail.com>
2018-08-15 21:05:20 -0600

    componente login creado

---
+++ App_Adminsitrador/App_Adminsitrador/MySQL/conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace App_Adminsitrador.MySQL
+{
+    class conexion
+    {
+        public static MySqlConnection obtenerConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection("server=35.202.163.254;database=pensum;Uid=admin;pwd=1234");
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
