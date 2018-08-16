126a4bdd63d7326b8b4d39db51eac8bd1803b168
Sergio De los Rios <sergiofernando23234@gmail.com>
2018-08-15 23:12:48 -0600

    nuevo web
    nuevo

--- conexion/conexion/Form1.cs
+++ conexion/conexion/Form1.cs
@@ -21,25 +21,52 @@ namespace Conexion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MySqlConnection conexion = new MySqlConnection("server=35.202.163.254;database=pensum;uid=admin;pwd=1234");
+            //MySqlConnection conexion = new MySqlConnection("server=35.202.163.254;database=pensum;uid=admin;pwd=1234");
             String usuario = textBox1.Text;
             String pass = textBox2.Text;
-            conexion.Open();
+            //conexion.Open();
 
-            MySqlCommand comando = new MySqlCommand("select nombre from usuario where usuario = '"+usuario+"' and password = '"+pass+"';", conexion);
-            MySqlDataReader myreader = comando.ExecuteReader();
-            myreader.Read();
-            try
+            //MySqlCommand comando = new MySqlCommand("select nombre from usuario where usuario = '"+usuario+"' and password = '"+pass+"';", conexion);
+            //MySqlDataReader myreader = comando.ExecuteReader();
+            //myreader.Read();
+            //try
+            //{
+            //    String strnombre = myreader["nombre"].ToString();
+            //    MessageBox.Show("Bien");
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show("Mal");
+            //}
+            Boolean devolver=Comprobar(usuario,pass);
+            if (devolver)
             {
-                String strnombre = myreader["nombre"].ToString();
                 MessageBox.Show("Bien");
             }
-            catch(Exception ex)
+            else
             {
                 MessageBox.Show("Mal");
             }
             
             
+        }
+        public Boolean Comprobar(String usuario,String pass)
+        {
+            MySqlConnection conexion = new MySqlConnection("server=35.202.163.254;database=pensum;uid=admin;pwd=1234");
+            conexion.Open();
+            MySqlCommand comando = new MySqlCommand("select nombre from usuario where usuario = '" + usuario + "' and password = '" + pass + "';", conexion);
+            MySqlDataReader myreader = comando.ExecuteReader();
+            myreader.Read();
+            try
+            {
+                String strnombre = myreader["nombre"].ToString();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
         }
 
         private void label2_Click(object sender, EventArgs e)
---
+++ conexion/conexion_test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Conexion;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Conexion.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void ComprobarTest()
+        {
+            Form1 form = new Form1();
+            Boolean resultado=form.Comprobar("Admin","12345");
+            Assert.ReferenceEquals(resultado,false);
+        }
+    }
+}
+
+namespace conexion_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
